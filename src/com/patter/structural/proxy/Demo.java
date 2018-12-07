1da3e8f5f10b76ba2aae2366484651e814268322
CkFang <1576644642@qq.com>
2018-12-07 15:25:51 +0800

    123

--- src/com/patter/structural/proxy/Demo.java
+++ src/com/patter/structural/proxy/Demo.java
@@ -4,7 +4,7 @@ package com.patter.structural.proxy;
  * 在代理模式（Proxy Pattern）中，一个类代表另一个类的功能。
  * 这种类型的设计模式属于结构型模式。
  * 在代理模式中，我们创建具有现有对象的对象，以便向外界提供功能接口。
- * @author EricMan178
+ * @author EricMan1781
  *
  */
 public class Demo {
@@ -17,5 +17,6 @@ public class Demo {
 		proxy.bookTicket();
 		proxy.sing();
 		proxy.cellectMoney();
+		
 	}
 }
